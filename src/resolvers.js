37fa8091bf27e55872e65f86207e23b2b4f393aa
François Ulrich <fulrich3@gmail.com>
2020-11-28 17:29:54 +0100

    first commit

---
+++ src/resolvers.js
@@ -0,0 +1,20 @@
+import {Cat} from './models/Cats';
+
+export const resolvers = {
+    Query: {
+        hello: () => "hello",
+        cats: () => Cat.find()
+    },
+
+    Mutation: {
+        createCat: async (_, {name}) => {
+            const kitty = new Cat({ name });
+
+            await kitty.save()
+
+            return kitty;
+        }
+    },
+
+}
+
